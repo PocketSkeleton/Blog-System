f46dfabfd6ecc6a4d4c22b106cacaefa5df24ee2
PocketSkeleton <dario.o.rechtman@gmail.com>
2023-07-20 13:12:21 +0700

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myDiv = document.getElementById("blogPage");
+const myDiv = document.getElementById("blogs");
 
 fetch('https://jsonplaceholder.typicode.com/posts/')
   .then((response) => response.json())
@@ -11,21 +11,44 @@ fetch('https://jsonplaceholder.typicode.com/posts/')
         title.setAttribute("class", "blogTitle");
         title.innerHTML = response[i]["title"];
 
+        let author = document.createElement("p");
+        author.setAttribute("class", "blogAuthor");
+        author.innerHTML = "User " + response[i]["userId"];
+
         let body = document.createElement("p");
         body.setAttribute("class", "blogBody");
-        body.innerHTML = response[i]["body"];
+        body.innerHTML = response[i]["body"]+'... <a>read more</a>';
 
-        blog.appendChild(title);
-        blog.appendChild(body);
-        myDiv.appendChild(blog);
-        console.log(response[i])
+        blog.append(title);
+        blog.append(author);
+        blog.append(body);
+        myDiv.append(blog);
     }
   })
 
 
 
+function writeBlog(c, t) {
+  let blog = document.createElement("div");
+  blog.setAttribute("class", "blog");
 
+  let title = document.createElement("h1");
+  title.setAttribute("class", "blogTitle");
+  title.innerHTML = t;
 
+  let author = document.createElement("p");
+  author.setAttribute("class", "blogAuthor");
+  author.innerHTML = "You";
+
+  let body = document.createElement("p");
+  body.setAttribute("class", "blogBody");
+  body.innerHTML = c;
+
+  blog.append(title);
+  blog.append(author);
+  blog.append(body);
+  myDiv.prepend(blog);
+}
 
 
 
@@ -37,8 +60,13 @@ document.getElementById("create").addEventListener("click", function() {
     document.getElementById("createBox").classList.add("open-popup")
 })
 
-document.getElementById("form").addEventListener("submit", function() {
+document.getElementById("form").addEventListener("submit", function(event) {
+  
+    event.preventDefault();  
+    writeBlog(document.getElementById("blogContent").value, document.getElementById("title").value);
     document.getElementById("createBox").classList.remove("open-popup")
+    document.getElementById("blogContent").value = '';
+    document.getElementById("title").value = '';
 })
 
 document.getElementById("closeButton").addEventListener("click", function() {
